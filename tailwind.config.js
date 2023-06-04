4ee532ac612c0df87f595145df32a755871604d6
9Out <nirot.ahmadi2004@gmail.com>
2023-06-04 12:15:30 +0700

    HPC Membuat web 2023

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./dist/script/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#FD636B",
+        primary: "#0c1117",
+        secondary: "#FD636B",
+      },
+      screens: {
+        sm: "375px",
+        "2xl": "1439px",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "2rem",
+        sm: "2rem",
+        md: "3rem",
+        x1: "6rem",
+      },
+    },
+  },
+  plugins: [],
+}
+
